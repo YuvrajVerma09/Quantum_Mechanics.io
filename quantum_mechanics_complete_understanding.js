e59b86466dc4a23ce8e62432b582fb0189432c29
King Of The World <98012258+YuvrajVerma09@users.noreply.github.com>
2024-05-11 14:40:42 +1000

    Add files via upload

---
+++ quantum_mechanics_complete_understanding.js
@@ -0,0 +1,52 @@
+function myFunction() {
+    var element = document.getElementById("navigation");
+    element.classList.toggle("nav_lightmode");
+ }
+ function myFunction2() {
+    var element = document.getElementById("body");
+    element.classList.toggle("dark-mode");
+ }
+ function myFunction3() {
+    var element = document.getElementById("span");
+    element.classList.toggle("span2");
+ }
+ function myFunction4() {
+    var element = document.getElementById("content");
+    element.classList.toggle("colourpage_lightmode");
+ }
+ function myFunction5() {
+    var element = document.getElementById("heading");
+    element.classList.toggle("h1_lightmode");
+ }
+ function myFunction6() {
+    var element = document.getElementById("quantum");
+    element.classList.toggle("Header_lightmode");
+ }
+ function myFunction7() {
+    var element = document.getElementById("paragraph1");
+    element.classList.toggle("paragraph1_lightmode");
+ }
+ function myFunction8() {
+    var element = document.getElementById("paragraph2");
+    element.classList.toggle("paragraph2_lightmode");
+ }
+ function myFunction9() {
+    var element = document.getElementById("bodytext");
+    element.classList.toggle("bodytext_lightmode");
+ }
+ function myFunction10() {
+    var element = document.getElementById("video");
+    element.classList.toggle("video_lightmode");
+ }
+ function myFunction11() {
+    var element = document.getElementById("footer");
+    element.classList.toggle("footer_lightmode");
+ }
+ function myFunction12() {
+    var element = document.getElementById("sources");
+    element.classList.toggle("footer_lightmode");
+ }
+ function myFunction13() {
+    var element = document.getElementById("sources2");
+    element.classList.toggle("footer_lightmode");
+ }
